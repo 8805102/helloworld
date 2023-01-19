8714d5f0b55d518a1a728c8b6e49fcd9baac2731
mark kradel <mark@zeva.us>
2023-01-19 14:32:05 -0500

    a2

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! A1");
+            Console.WriteLine("Hello, World! A2");
         }
     }
 }
