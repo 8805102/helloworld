9b6a17e494eb4da0b19adc7487ac6a16fbf7728a
Mark Kradel <mark@zeva.us>
2023-05-04 14:01:17 -0400

    x

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World! b5");
-            Console.WriteLine(" Meatballs");
+            Console.WriteLine("Hello, World! 8");
+            Console.WriteLine(" East meets West");
         }
     }
 }
